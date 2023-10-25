baf470cd5c556886a8fd648f12ad6468c8ce246b
Shivam5278 <st527874@gmail.com>
2023-10-26 03:32:28 +0530

    update

---
+++ Two_Cars/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+function App(props) {
+  return (
+    <View style={styles.container}>
+      <Text> Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default App;
